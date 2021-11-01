4343f75ba9121e907def23e1147c8702df5f094e
Mohak Talreja <92052668+mohak0011@users.noreply.github.com>
2021-11-01 23:54:39 +0530

    Remove node_modules folder

--- metauniverse/src/App.js
+++ metauniverse/src/App.js
@@ -1,10 +1,11 @@
+import Home from "./pages/home/Home";
 import React from 'react';
 function App() {
-  return (
-    <div >
-    Hello Browser
-    </div>
-  );
+  return(  
+    <div>
+    <Home/>
+  </div>
+  )
 }
 
 export default App;
