1ffa8bf69fcf104901b9d28db34181277f994984
Mohak Talreja <92052668+mohak0011@users.noreply.github.com>
2021-11-01 21:02:41 +0530

    feat: initial commit for setting up the project

---
+++ metauniverse/README.md
@@ -0,0 +1,3 @@
+# introduction
+ a social media app similar to facebook currently known as metauniverse 
+ # socialmedia-app
---
+++ metauniverse/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div >
+    Hello Browser
+    </div>
+  );
+}
+
+export default App;
