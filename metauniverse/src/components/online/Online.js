917ee006048c22be1b1c56ceb499fdf455f4932b
Mohak Talreja <92052668+mohak0011@users.noreply.github.com>
2021-11-09 21:12:01 +0530

    Feat : creating an feature for friends who are online with styling

---
+++ metauniverse/src/components/online/Online.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Online.css';
+
+function Online({user}) {
+    return (
+        <li className="rightbarFriend">
+        <div className="rightbarProfileImgContainer">
+          <img className="rightbarProfileImg" src={user.profilePicture} alt="" />
+          <span className="rightbarOnline"></span>
+        </div>
+        <span className="rightbarUsername">{user.username}</span>
+      </li>
+    )
+}
+
+export default Online
