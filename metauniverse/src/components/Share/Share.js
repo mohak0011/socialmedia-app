107908b735f392472f7640a0b0127de8829421e6
Mohak Talreja <92052668+mohak0011@users.noreply.github.com>
2021-11-09 20:42:51 +0530

    feat : Adding share inside feed

--- metauniverse/src/components/Share/Share.js
+++ metauniverse/src/components/Share/Share.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Share.css';
-import {PermMedia} from '@material-ui/icons';
+import {PermMedia,Label,Room, EmojiEmotions} from '@material-ui/icons';
 
 function Share() {
     return (
@@ -17,11 +17,24 @@ function Share() {
                     <PermMedia classname='shareIcon'/>
                         <span className='shareOptionText'>Photo or Video</span>
                     </div>
+                    <div className="shareOption">
+                    <Label htmlColor="blue" className="shareIcon"/>
+                    <span className="shareOptionText">Tag</span>
                 </div>
+                <div className="shareOption">
+                    <Room htmlColor="green" className="shareIcon"/>
+                    <span className="shareOptionText">Location</span>
+                </div>
+                <div className="shareOption">
+                    <EmojiEmotions htmlColor="goldenrod" className="shareIcon"/>
+                    <span className="shareOptionText">Feelings</span>
+                </div>  
+                </div>
+                <button className="shareButton">Share</button>
             </div>
         </div>
         </div>
-    )
+    );
 }
 
 export default Share
