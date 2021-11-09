37e1e802f8397a9b3cb478bcc449bdc8f00da943
Mohak Talreja <92052668+mohak0011@users.noreply.github.com>
2021-11-09 15:43:19 +0530

    feat: setting up the body components

---
+++ metauniverse/src/components/sidebar/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sidebar.css';
+
+function Sidebar() {
+    return (
+        <div className='sidebar'>
+            SideBar
+        </div>
+    )
+}
+
+export default Sidebar
--- metauniverse/src/pages/home/Home.js
+++ metauniverse/src/pages/home/Home.js
@@ -1,9 +1,20 @@
-import React from 'react'
-import Topbar from '../../components/topbar/Topbar'
+import React from 'react';
+import './Home.css';
+import Topbar from '../../components/topbar/Topbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Feed from '../../components/feed/Feed';
+import Rightbar from '../../components/rightbar/Rightbar';
 
 function Home() {
     return (
-       <Topbar/>
+         <>
+        <Topbar/>
+        <div className="homeContainer">
+        <Sidebar/>
+        <Feed/>
+        <Rightbar/>
+        </div>
+        </>
     )
 }
 
