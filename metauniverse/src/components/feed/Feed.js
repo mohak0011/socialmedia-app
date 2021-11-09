71f4d98f8f825517e0dcebf6c5ff7ca37bc9e6a9
Mohak Talreja <92052668+mohak0011@users.noreply.github.com>
2021-11-09 21:14:28 +0530

    Completing feed component and fetching data

--- metauniverse/src/components/feed/Feed.js
+++ metauniverse/src/components/feed/Feed.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Share from '../Share/Share';
 import './Feed.css';
+import Post from "../post/Post";
+import { Posts } from "../../dummyData";
 
 function Feed() {
     return (
         <div className='feed'>
             <div className="feedWrapper">
                 <Share/>
+                {Posts.map((p) => (
+          <Post key={p.id} post={p} />
+        ))}
             </div>
         </div>
     )
